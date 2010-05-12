963f9f108112a73d4572b43be24d4bfefd36b2eb
Silvano Stralla <silvano.stralla@sistrall.it>
2010-05-12 12:38:54 +0200

    Process pages keeping in count of the presence of a SimplePasswordPage in the ancestors of che currently processed page

--- lib/site_controller_ext.rb
+++ lib/site_controller_ext.rb
@@ -10,13 +10,14 @@ module SimplePassword
     module InstanceMethods
     
       protected
-  
+      
+      # Process page keeping in count of the presence of a SimplePasswordPage in the ancestors of che currently processed page
       def process_page_with_simple_password(page)
-        if page.class == SimplePasswordPage
-          if authenticate_with_http_basic { |u, p| u == page.user && p == page.password }
+        if simple_password_page = ([page] + page.ancestors).find { |p| p.is_a?(SimplePasswordPage) }
+          if authenticate_with_http_basic { |u, p| u == simple_password_page.user && p == simple_password_page.password }
             page.process(request, response)
           else
-            request_http_basic_authentication( page.realm )
+            request_http_basic_authentication(simple_password_page.realm)
           end
         else
           page.process(request, response)
